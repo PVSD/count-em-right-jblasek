2742c9c480d99e9bd2fa02dfbc0df436cb93bd8f
jblasek <jacob.blasek1220@gmail.com>
2018-10-17 21:02:18 -0400

    I am confused, why did you give us the code if the code is what the answer is?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner (System.in);
+        String inp, sp[];
+
+        do {
+            System.out.print("Type in a sentence and press ENTER.");
+            inp = sc.nextLine();
+            inp = inp + "X";
+            inp = inp.toUpperCase();
+
+            sp = inp.split("S\\s*A");
+
+            if (inp.equals("EXITX"))
+                break;
+
+            System.out.println("There are " + (sp.length - 1) + " occurrences");
+            System.out.println("");
+        }
+
+        while (1 == 1) ;
+
+            System.out.println("Finished");
+        }
+
     }
-}
+
+
